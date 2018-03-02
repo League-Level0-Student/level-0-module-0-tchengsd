fcf6ae94c5e155b66b9ebda34000e94c6f598b9a
league <league@wts13.attlocal.net>
2018-03-01 19:58:03 -0800

    KnockKnock, LastSummer

---
+++ src/KnockKnock.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class KnockKnock {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "Knock, knock.");
+	JOptionPane.showMessageDialog(null, "Who’s there?");
+	JOptionPane.showMessageDialog(null, "Noah.");
+	JOptionPane.showMessageDialog(null, "Noah who?");
+	JOptionPane.showMessageDialog(null, "Noah good place we can get something to eat?");
+}
+}
